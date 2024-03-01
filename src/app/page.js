c5a89f3b5d22a5c5c3dc46d54dde28a4daa96921
rrscode <ricosantos86@gmail.com>
2024-03-01 14:59:21 -0800

    makeCount

--- src/app/page.js
+++ src/app/page.js
@@ -20,78 +20,112 @@ const Page = () => {
     })
   }, []);
 
-// // const makeCount = () => {  
-  
-// // }
+
  
-//   const sortedCars = cars.sort();
-
-//   const itemListByMake = sortedCars.map((x) => {
-
-//     const carMake = [{ 
-//       make: x.make,
-//       value: 0
-//     }]
-    
-//     return carMake
-//   });
-  
-//   const makeCount = [];
-
-//   itemListByMake.forEach((x) => {
-//     makeCount[y] = {
-//       make: x.make,
-//       value: x.value
-//     }
-//     if (x = makeCount[y]){
-//        y.value += 1
-//     } else {y.value = 1}
-    
-//   })
-  
-//   console.log(makeCount)
-//   // if (make = carMake.make) {
-//   //   carMake.value += 1
-//   // } else { carMake.value = 1}
-  
-//   // const makeList = itemListByMake.forEach((x) => {
-//   //     itemListByMake.filter()
-//   //   })
-
-
-
-  
-
-  
-
-  // itemListByMake.sort()
-  
-  // // const counter = listOfMakers.map((x) => {
-  //   //   if (x == prev)
-  //   // })
-    
-  // console.log(itemListByMake)
-  // }
-
-  const newData = [];
-  cars.map((car) => {
-    /**
-     * check if newData Array contains object with make that matches current car make
-     * if the newData array contains match, increment count of that matching newData array object
-     * if no match, create new object in newData array with matching make and set count to 1
-     */
-
-    newData.length === 0 ? newData.push({make: car.make, count: 0}) :
-    newData.forEach((dataItem, dataIndex) => {
-      if (dataItem.make === car.make) {
-        console.log(newData[dataIndex])
-        // console.log(dataIndex)
-      }
-      // newData.push({make: car.make, count: 1})
-    });
+ 
+  const listByMake = cars.map((x) => {
+    return x.make
+  })
+  const sortedListByMake = listByMake.sort();
+  const WIPData = sortedListByMake.map((x) => {
+    let makeData = {
+      make: x,
+      count: 1
+    }
+    return makeData
   });
-  // console.log(newData)
+  const WIPData1 = WIPData.map((x, y, arr) => {
+    if (y == 0){
+      return  x
+    }else if (x.make == arr[y-1].make){
+      x.count = x.count + arr[y-1].count;
+      arr[y-1].count = 0;
+      return x
+    } else {
+      return x
+    }
+   })
+   const makeCount = WIPData1.filter((x) => {
+    if (x.count > 0)
+    return x
+   })
+
+console.log(makeCount)
+
+
+
+
+
+
+
+
+/** 
+const makeCount = () => {  
   
+}
+ 
+const sortedCars = cars.sort();
+
+const itemListByMake = sortedCars.map((x) => {
+
+  const carMake = [{ 
+       make: x.make,
+     value: 0
+     }]
+    
+     return carMake
+  });
+  
+   const makeCount = [];
+
+   itemListByMake.forEach((x) => {
+     makeCount[y] = {
+       make: x.make,
+       value: x.value
+     }
+    if (x = makeCount[y]){
+        y.value += 1
+     } else {y.value = 1}
+    
+   })
+  
+   
+    if (make = carMake.make) {
+      carMake.value += 1
+   } else { carMake.value = 1}
+  
+    const makeList = itemListByMake.forEach((x) => {
+        itemListByMake.filter()
+     })
+
+
+
+  
+
+  
+
+   itemListByMake.sort()
+  
+    const counter = listOfMakers.map((x) => {
+   if (x == prev)
+   })
+    
+  
+   }
+
+   const newData = [];
+   cars.map((car) => {
+     newData.length === 0 ? newData.push({make: car.make, count: 0}) :
+   newData.forEach((dataItem, dataIndex) => {
+     if (dataItem.make === car.make) {
+       
+      // }
+      // newData.push({make: car.make, count: 1})
+     });
+   });
+ 
+*/
+
   return (
     <>
       <h1>JC's Garage</h1>
